deed5b4761d2bdf0e4ab6c55f6c2eeccd27c7998
Anna Wegelius <anweg@Annas-MacBook-Air.local>
2016-12-14 11:50:50 +0100

    initial commit

---
+++ client/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+//import {module} from "@angular/upgrade/src/angular_js";
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html'
+})
+
+export class AppComponent {
+}
